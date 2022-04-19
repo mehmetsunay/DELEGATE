6887981b8bb56075222089f213ebe27094aecc60
Mehmet Sunay <mehmetsunay@windowslive.com>
2022-04-19 14:21:16 +0300

    DELEGATE
    DELEGATE

---
+++ ConsoleDelegate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleDelegate
+{
+    class Program
+    {
+
+        /*
+         Delegateler bir nevi metod pointerlarıdır
+         Yani onları yönetirler 
+         Delegete tipinden bir nesne türetip rahatlıkla işlemler yapabilmemizi sağlarlar
+         ..................................................................
+
+        Aşağıdaki örnekte  çevre ve alan hesabı yapan metodlarımı , ana metodum içerisinde delegeteden
+       nesne turetip parametre geçirerek kullandım ...
+        Ayrıca Delegetın parametresi içindeki değişkenlerinde aşağıdakilerle aynı olma zorunluluğu yoktur. 
+        
+         */
+
+        delegate double Dikdortgen(double x, double y);
+        static void Main(string[] args)
+        {
+            Dikdortgen test = new Dikdortgen(alan);
+            Console.WriteLine(test(3, 8));
+            Console.ReadLine();
+
+        }
+        static double cevre(double a,double b)
+        {
+
+            return 2 * (a + b);
+
+        }
+
+        static double alan(double a ,double b)
+        {
+
+            return a * b;
+
+        }
+
+      
+
+
+
+    }
+}
